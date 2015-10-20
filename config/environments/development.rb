f9d71d34f99ab4be25773c5f6179a2acfe785bb8
maangie <maangie906@gmail.com>
2015-10-20 18:42:08 +0900

    set of SMTP server.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,4 +42,13 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    authentication: :plain,
+    user_name: Rails.application.secrets.gmail_address,
+    password: Rails.application.secrets.gmail_password
+  }
 end
